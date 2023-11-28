ff8c4b03d023ce2facfc86cd9fc0e127fa01f7f6
MinhBang1000 <bradonleminhbang1@gmail.com>
2023-11-28 09:27:28 +0700

    DAM_0001: Inital Project

---
+++ src/main/java/ctu/cit/se/dam_service/DamServiceApplication.java
@@ -0,0 +1,13 @@
+package ctu.cit.se.dam_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamServiceApplication.class, args);
+	}
+
+}
